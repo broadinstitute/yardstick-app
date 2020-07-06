1830e4994308bea29b267f4763012d2ea9fab76d
Allen Goodman <allen.goodman@icloud.com>
2020-07-06 18:12:41 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,40 @@
-instance_segmentation_task = Task.create(
-    {
-        description: "Classify, detect, and delineate discrete objects.",
-        name: "Instance segmentation",
-    }
-)
+task_0 = Task.create(name: "instance segmentation")
+task_1 = Task.create(name: "object detection")
+task_2 = Task.create(name: "semantic segmentation")
+task_3 = Task.create(name: "super-resolution")
 
-object_detection_task = Task.create(
-    {
-        description: "Classify and detect discrete objects.",
-        name: "Object detection",
-    }
-)
+challenge_0 = Challenge.create(name: "eukaryote components", task: task_0)
+challenge_1 = Challenge.create(name: "eukaryote components", task: task_1)
+challenge_2 = Challenge.create(name: "eukaryote components", task: task_2)
 
-semantic_segmentation_task = Task.create(
-    {
-        description: "Classify every pixel in an image with an appropriate category.",
-        name: "Semantic segmentation",
-    }
-)
+version_0 = Version.create(name: "1.0.0", challenge: challenge_0)
+version_1 = Version.create(name: "1.0.0", challenge: challenge_1)
+version_2 = Version.create(name: "1.0.0", challenge: challenge_2)
 
-super_resolution_task = Task.create(
-    {
-        description: "Increase the magnification of an image.",
-        name: "Super-resolution",
-    }
-)
+metric_0 = Metric.create(name: "mean average precision (mAP)", version: version_0)
+metric_1 = Metric.create(name: "mean average precision (mAP)", version: version_1)
+metric_2 = Metric.create(name: "mean average precision (mAP)", version: version_2)
 
-Challenge.create(
-    {
-        description: "",
-        name: "Eukaryote components",
-        task: instance_segmentation_task,
-    }
-)
+training_partition_0 = Partition.create(endpoint: "training.tar.gz")
+training_partition_1 = Partition.create(endpoint: "training.tar.gz")
+training_partition_2 = Partition.create(endpoint: "training.tar.gz")
 
-Challenge.create(
-    {
-        description: "",
-        name: "Eukaryote components",
-        task: object_detection_task,
-    }
-)
+validation_partition_0 = Partition.create(endpoint: "validation.tar.gz")
+validation_partition_1 = Partition.create(endpoint: "validation.tar.gz")
+validation_partition_2 = Partition.create(endpoint: "validation.tar.gz")
 
-Challenge.create(
-    {
-        description: "",
-        name: "Eukaryote components",
-        task: semantic_segmentation_task,
-    }
-)
+test_partition_0 = Partition.create(endpoint: "test.tar.gz")
+test_partition_1 = Partition.create(endpoint: "test.tar.gz")
+test_partition_2 = Partition.create(endpoint: "test.tar.gz")
+
+version_0.training_partition = training_partition_0
+version_0.validation_partition = validation_partition_0
+version_0.test_partition = test_partition_0
+
+version_1.training_partition = training_partition_1
+version_1.validation_partition = validation_partition_1
+version_1.test_partition = test_partition_1
+
+version_2.training_partition = training_partition_2
+version_2.validation_partition = validation_partition_2
+version_2.test_partition = test_partition_2
