cf18dcc54d52c2dc0a508333bcf14840db57e98e
Allen Goodman <allen.goodman@icloud.com>
2020-07-06 19:56:21 -0400

    react

--- app/javascript/components/NavigationDrawer.tsx
+++ app/javascript/components/NavigationDrawer.tsx
@@ -2,25 +2,23 @@ import * as React from "react";
 import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import {useStyles} from "./NavigationDrawer.css";
 import ListSubheader from "@material-ui/core/ListSubheader";
 
 type NavigationDrawerProps = {
+    challenges: Array<{id: string, name: string}>
     onClose: () => void
     open: boolean
-    challenges: Array<{id: string, name: string}>
     tasks: Array<{id: string, name: string, task_id: string}>
 }
 
-const NavigationDrawer = ({onClose, open, tasks}: NavigationDrawerProps) => {
+const NavigationDrawer = ({challenges, onClose, open, tasks}: NavigationDrawerProps) => {
     const classes = useStyles();
 
-    const challenges = tasks.map(({id, name}) => {
+    const c = tasks.map(({id, name}) => {
         const subheader = (
             <ListSubheader>
                 {name}
@@ -48,22 +46,14 @@ const NavigationDrawer = ({onClose, open, tasks}: NavigationDrawerProps) => {
                 >
                     <div className={classes.toolbar} />
                     <Divider />
-                    <List>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-
-                            <ListItemText primary="Example" />
-                        </ListItem>
-                    </List>
+                    {c}
                 </Drawer>
             </Hidden>
             <Hidden xsDown implementation="css">
                 <Drawer classes={{paper: classes.paper}} open variant="permanent">
                     <div className={classes.toolbar} />
                     <Divider />
-                    {challenges}
+                    {c}
                 </Drawer>
             </Hidden>
         </nav>
