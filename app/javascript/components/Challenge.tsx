cbfe9d0cb053b6cdfc73aba408b5d9d5b588d798
Allen Goodman <allen.goodman@icloud.com>
2020-07-05 21:02:36 -0400

    controllers

---
+++ app/javascript/components/Challenge.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+type ChallengeProps = {}
+
+const Challenge = ({}: ChallengeProps) => {
+    return <div/>
+};
+
+export default Challenge;
---
+++ app/javascript/components/Submission.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+type SubmissionProps = {}
+
+const Submission = ({}: SubmissionProps) => {
+    return <div/>
+};
+
+export default Submission;
