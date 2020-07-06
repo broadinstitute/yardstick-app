c85e3d8c6b05756295bb9f6b87232701d1e6c9ed
Allen Goodman <allen.goodman@icloud.com>
2020-07-05 20:39:03 -0400

    routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,13 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show]
-
-  # GET /tasks
-  # GET /tasks.json
   def index
     @tasks = Task.all
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
+    @task = Task.find(params[:id])
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def task_params
       params.fetch(:task, {})
     end
