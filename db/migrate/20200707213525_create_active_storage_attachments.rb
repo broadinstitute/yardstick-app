5f3a84e33cd6430ef9a8ff85410330c738a02970
Allen Goodman <allen.goodman@icloud.com>
2020-07-07 17:36:38 -0400

    migrations

--- db/migrate/20200707213525_create_active_storage_attachments.rb
+++ db/migrate/20200707213525_create_active_storage_attachments.rb
@@ -1,6 +1,14 @@
 class CreateActiveStorageAttachments < ActiveRecord::Migration[6.0]
   def change
-    create_table :active_storage_attachments do |t|
+    create_table :active_storage_attachments do |table|
+      table.string     :name,     null: false
+      table.references :record,   null: false, polymorphic: true, index: false
+      table.references :blob,     null: false
+
+      table.datetime :created_at, null: false
+
+      table.index %i[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+      table.foreign_key :active_storage_blobs, column: :blob_id
     end
   end
 end
