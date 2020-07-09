1aaed93573f7736ba254836ef8381fec5a4e403c
Allen Goodman <allen.goodman@icloud.com>
2020-07-09 11:18:50 -0400

    migration cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 2020_07_09_145248) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "name", null: false
     t.bigint "blob_id", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
+    t.string "name", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -38,23 +38,23 @@ ActiveRecord::Schema.define(version: 2020_07_09_145248) do
   end
 
   create_table "challenges", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "task_id"
     t.string "name", default: "", null: false
     t.string "slug", default: "", null: false
     t.text "description", default: "", null: false
-    t.uuid "task_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["task_id"], name: "index_challenges_on_task_id"
   end
 
   create_table "metrics", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "version_id"
     t.string "abbreviation", default: "", null: false
     t.string "href", default: "", null: false
     t.string "name", default: "", null: false
     t.text "description", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.uuid "version_id"
     t.index ["version_id"], name: "index_metrics_on_version_id"
   end
 
@@ -77,12 +77,12 @@ ActiveRecord::Schema.define(version: 2020_07_09_145248) do
   end
 
   create_table "submissions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id"
+    t.uuid "version_id"
     t.string "name", default: "", null: false
     t.text "description", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.uuid "user_id"
-    t.uuid "version_id"
     t.index ["user_id"], name: "index_submissions_on_user_id"
     t.index ["version_id"], name: "index_submissions_on_version_id"
   end
@@ -113,14 +113,14 @@ ActiveRecord::Schema.define(version: 2020_07_09_145248) do
   end
 
   create_table "versions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "challenge_id"
+    t.uuid "test_partition_id"
+    t.uuid "training_partition_id"
+    t.uuid "validation_partition_id"
     t.string "name", default: "", null: false
     t.text "description", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.uuid "challenge_id"
-    t.uuid "training_partition_id"
-    t.uuid "validation_partition_id"
-    t.uuid "test_partition_id"
     t.index ["challenge_id"], name: "index_versions_on_challenge_id"
     t.index ["test_partition_id"], name: "index_versions_on_test_partition_id"
     t.index ["training_partition_id"], name: "index_versions_on_training_partition_id"
