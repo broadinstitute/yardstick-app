a6418d24251aa9cb790520cfc5ef13ad72a9a962
Allen Goodman <allen.goodman@icloud.com>
2020-07-11 19:22:07 -0400

    react routes

---
+++ app/javascript/components/User.css.ts
@@ -0,0 +1,7 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const styles = {};
+
+const style = (_: Theme) => createStyles(styles);
+
+export const useStyles = makeStyles(style);
